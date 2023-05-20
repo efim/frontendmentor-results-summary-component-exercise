c127fcb4cc84a6bb89dc55c4f6b4598fbf49d399
efim <efim.nefedov@nordigy.ru>
2023-05-20 16:05:38 +0400

    feat: styling large media

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -19,44 +19,71 @@ object Main {
   def resultsSummary(results: List[CategoryResult]): Element = {
     val totalScore = results.map(_.score).sum / results.size
     div(
-      className := "flex flex-col items-center h-screen",
+      className := "h-full w-screen h-screen flex flex-row items-center justify-center ",
       div(
-        className := "flex flex-col items-center w-full rounded-b-[40px]",
-        className := "bg-gradient-to-b from-[#7643FF] to-[#2E2CE9]",
+        className := "flex flex-col items-center h-screen",
+        className := "lg:flex-row lg:h-[750px] lg:w-[1080px] lg:place-self-center lg:shadow-2xl rounded-[40px]  ",
+        renderTotal(totalScore),
+        renderSummary(results)
+      )
+    )
+  }
+
+  def renderTotal(totalScore: Int) = {
+    div(
+      className := "flex flex-col items-center w-full rounded-b-[40px] ",
+      className := "lg:rounded-[40px] lg:h-full lg:w-1/2 lg:justify-center ",
+      className := "bg-gradient-to-b from-[#7643FF] to-[#2E2CE9]",
+      p(
+        "Your Result",
+        className := "text-[#D4CAFF] text-2xl font-semibold p-8 ",
+        className := "lg:text-4xl lg:pb-14"
+      ),
+      div(
+        className := "rounded-full bg-blue-900 h-56 w-56 flex flex-col justify-center items-center p-14",
+        className := "lg:h-[300px] lg:w-[300px] lg:p-1",
+        className := "bg-gradient-to-b from-[#4A23CC] to-[#4734f0]",
         p(
-          "Your Result",
-          className := "text-[#D4CAFF] text-2xl font-semibold p-8 "
+          s"$totalScore",
+          className := "text-7xl font-extrabold text-white ",
+          className := "lg:text-8xl"
         ),
-        div(
-          className := "rounded-full bg-blue-900 h-56 w-56 flex flex-col justify-center items-center p-14",
-          className := "bg-gradient-to-b from-[#4A23CC] to-[#4734f0]",
-          p(s"$totalScore", className := "text-7xl font-extrabold text-white "),
-          p("of 100", className := "text-2xl font text-[#8a80ff] p-2")
-        ),
-        div(
-          className := "flex flex-col items-center",
-          p("Great", className := "p-6 text-white text-4xl font-bold"),
-          p(
-            "You scored higher than 65% of the people who have taken these tests.",
-            className := "text-[#B4B6FF] text-2xl font-semibold text-center pb-16 px-32 "
-          )
+        p("of 100",
+          className := "text-2xl font text-[#8a80ff] p-2",
+          className := "lg:font-bold"
         )
       ),
       div(
-        className := "flex flex-col items-start w-full px-10",
-        p("Summary", className := "font-bold text-2xl py-8"),
-        results.map(renderCategoryScore(_)),
-        div(
-          className := "py-7 w-full h-full",
-          button(
-            "Continue",
-            className := "text-white text-2xl font-bold bg-[#303B59] h-20 w-full rounded-full"
-          )
+        className := "flex flex-col items-center",
+        p("Great",
+          className := "p-6 text-white text-4xl font-bold",
+          className := "lg:text-5xl lg:p-8"
+        ),
+        p(
+          "You scored higher than 65% of the people who have taken these tests.",
+          className := "text-[#B4B6FF] text-2xl font-semibold text-center pb-16 px-32 ",
+          className := "lg:px-20 lg:text-[28px] lg:font-normal"
         )
       )
     )
   }
 
+  def renderSummary(results: List[CategoryResult]) = {
+    div(
+      className := "flex flex-col w-full px-10 lg:px-14",
+      className := " lg:w-1/2 lg:h-full lg:rounded-r-[40px] lg:relative",
+      p("Summary", className := "font-bold text-2xl py-8 lg:pt-12 lg:text-4xl"),
+      results.map(renderCategoryScore(_)),
+      div(
+        className := "py-7 w-full h-full",
+        className := "lg:pt-14",
+        button(
+          "Continue",
+          className := "text-white text-2xl font-bold bg-[#303B59] h-20 w-full rounded-full",
+        )
+      )
+    )
+  }
   def renderCategoryScore(result: CategoryResult): Element = {
 
     val svgRaw = Fetch.get(result.iconPath).text.map(_.data)
@@ -74,13 +101,14 @@ object Main {
         className := "grid grid-cols-7 w-full h-20 rounded-xl items-center ",
         styleAttr := s"--custom-bg: ${result.bgColor}; --custom-highlight: ${result.highlightColor}",
         className := "bg-[--custom-bg]",
+        className := "lg:rounded-2xl",
         child <-- svgIcon,
         p(
           className := "col-span-4 text-2xl text-[--custom-highlight] font-bold ",
           result.name
         ),
         div(
-          className := "col-span-2 justify-self-center text-2xl flex flex-row",
+          className := "col-span-2 justify-self-center text-2xl flex flex-row lg:pr-6",
           p(s"${result.score}", className := "font-semibold pr-2 "),
           p(" / 100", className := "text-gray-400 font-semibold")
         )
