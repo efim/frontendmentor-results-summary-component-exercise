c9f1815092383135a931e3d603b9a9f09224c05b
efim <efim.nefedov@nordigy.ru>
2023-05-20 17:55:17 +0400

    bug: fix Fetch path for static public content
    Vite changes pathdir, removes top level of "public"
    so in production build files would be available at path after 'public' and without leading '.'
    and in dev build the fetch request would also be rewritten to target
    files in 'public' dir

--- src/main/scala/example/Data.scala
+++ src/main/scala/example/Data.scala
@@ -14,28 +14,28 @@ object Data {
     CategoryResult(
       "Reaction",
       80,
-      "./assets/images/icon-reaction.svg",
+      "/assets/images/icon-reaction.svg",
       "#FFF6F5",
       "#DD767D"
     ),
     CategoryResult(
       "Memory",
       92,
-      "./assets/images/icon-memory.svg",
+      "/assets/images/icon-memory.svg",
       "#FFFBF2",
       "#E3B44A"
     ),
     CategoryResult(
       "Verbal",
       61,
-      "./assets/images/icon-verbal.svg",
+      "/assets/images/icon-verbal.svg",
       "#F2FBFA",
       "#29A784"
     ),
     CategoryResult(
       "Visual",
       72,
-      "./assets/images/icon-visual.svg",
+      "/assets/images/icon-visual.svg",
       "#F3F3FD",
       "#0E1987"
     )
